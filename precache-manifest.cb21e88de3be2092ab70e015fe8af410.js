7672084d223d8c897bda902a3468ddf6133e673e
Damien <dnyceboy@gmail.com>
2021-04-25 17:00:12 -0400

    Updates

---
+++ precache-manifest.cb21e88de3be2092ab70e015fe8af410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf7248462cb058638132ef2286ac745",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "941f1f4ba101f1ecfc6b",
+    "url": "/todolistreact/static/css/main.0bfb9e11.chunk.css"
+  },
+  {
+    "revision": "c450636df2cf4d102e5c",
+    "url": "/todolistreact/static/js/2.8bb4a0ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolistreact/static/js/2.8bb4a0ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "941f1f4ba101f1ecfc6b",
+    "url": "/todolistreact/static/js/main.9d1456b4.chunk.js"
+  },
+  {
+    "revision": "b614522d11368d31e470",
+    "url": "/todolistreact/static/js/runtime-main.b03fa772.js"
+  }
+]);
